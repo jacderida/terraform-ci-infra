360f3567462ad49a654d194ef143608c014be874
Chris O'Neil <chriso83@protonmail.com>
2022-11-06 23:04:47 +0000

    feat: remove webhook module and lambda
    The webhook module, which was adapted from the philips-labs repository, is removed to make way for a
    simpler setup.
    
    In the new setup, we can directly launch an EC2 instance, so the SQS queue is not necessary. We will
    also host two Lambda functions packaged as containers, so two repositories are created in our
    private container registry. This registry is something that's automatically available for any AWS
    account, so it doesn't need to be created with Terraform.

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 SHELL:=/bin/bash
 
 infrastructure:
-	(
-		cd lambda/webhook
-		yarn install
-		yarn run test
-		yarn dist
-	)
 	terraform init
 	terraform apply -auto-approve
 
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "subnet_name" {
 output "gha_runner_security_group_name" {
   value = aws_security_group.gha_runner_image_builder.id
 }
+
+output "gha_runner_create_instance_repository_url" {
+  value = aws_ecr_repository.gha_runner_create_instance.repository_url
+}
+
+output "gha_runner_terminate_instance_repository_url" {
+  value = aws_ecr_repository.gha_runner_terminate_instance.repository_url
+}

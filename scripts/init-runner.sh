1697101c1e0e2d92fcc72f008584c4638bf79093
Chris O'Neil <chriso83@protonmail.com>
2022-11-20 19:27:47 +0000

    feat: extend ami with cargo and musl
    Several packages are provided that allow the machine to perform a complete build of the
    `safe_network` repository, including test code.
    
    The Cargo installation symlinks all the binaries to a system-wide location. This is because I ran
    into some problems with the runner process not having a full login shell environment and I had to
    end up having a `source ~/.cargo/env` command in every `shell` step that used Cargo. We can just
    symlink the binaries to `/usr/local/bin` to avoid this.

--- scripts/init-runner.sh
+++ scripts/init-runner.sh
@@ -5,7 +5,10 @@ retry_count=1
 packages_installed="false"
 while [[ $retry_count -le 20 ]]; do
   echo "Attempting to install packages..."
-  sudo DEBIAN_FRONTEND=noninteractive apt install docker.io git -y
+  # All these packages are necessary for a full build of all safe_network code,
+  # including test binaries.
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+    build-essential docker.io git libssl-dev musl-tools pkg-config ripgrep unzip
   exit_code=$?
   if [[ $exit_code -eq 0 ]]; then
       echo "packages installed successfully"
@@ -26,3 +29,12 @@ fi
 
 sudo systemctl enable docker
 sudo usermod --groups docker ubuntu
+
+curl -L -O https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init
+chmod +x rustup-init
+./rustup-init --default-toolchain stable -y --no-modify-path
+# Make Cargo related binaries available in a system-wide location.
+# This prevents difficulties with having to source ~/.cargo/env in every shell
+# step in an actions workflow.
+sudo ln -s ~/.cargo/bin/* /usr/local/bin
+rustup target add x86_64-unknown-linux-musl
